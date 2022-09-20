2dd8e178a6f1bdd659c341ccea8c1892a0797583
dimeprog <dimejiawoyefa5@gmail.com>
2022-09-20 08:40:24 +0100

    bread crumb app start

---
+++ testprovider_course/lib/breadcrumbApp/bread_Homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BreadHomePage extends StatelessWidget {
+  BreadHomePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BreadCrumbApp"),
+      ),
+      body: Column(children: []),
+    );
+  }
+}
